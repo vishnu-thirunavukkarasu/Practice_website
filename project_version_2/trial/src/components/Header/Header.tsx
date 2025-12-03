5670d7ca09aca63b03652f98bf14a02007f51962
vishnu-thirunavukkarasu <vishnubenoit@gmail.com>
2025-12-03 22:16:39 +0100

    Add Home button with icon to Header component
    Introduced a Home button featuring a FontAwesome home icon to the Header component for improved navigation and UI clarity.

--- project_version_2/trial/src/components/Header/Header.tsx
+++ project_version_2/trial/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import './Header.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 interface HeaderProps {
     title: string;
@@ -9,7 +10,8 @@ function Header({ title }: HeaderProps) {
     return (
         <header className="webheader">
             <div>
-
+                <button><FontAwesomeIcon icon={faHome} style={{ marginRight: "8px" }} />
+                    Home</button>
             </div>
             <p className="indiHeaderTitle">{title}</p>
         </header>
